86f1b3692ae4180ac4b5e70895670b8708403ca1
chandra-gummaluru <chandra.gummaluru@outlook.com>
2015-11-30 19:45:12 -0500

    1.2.1
    -Added triggerMap enum and separate getTrigger method to get the raw
    value of the specified trigger

--- src/robot/F310_Joystick.java
+++ src/robot/F310_Joystick.java
@@ -11,31 +11,76 @@ public class F310_Joystick {
 
 	public enum Axis {
 		X, 
-		Y, 
-		Z;
+		Y;
 	}
-	
-	public enum Side {
+
+	public enum Stick {
 		LEFT,
 		RIGHT;
 	}
 
 	// does not contain all available buttons on the F310
 	public enum buttonMap {
-		LEFT_STICK(12, 10), 
-		RIGHT_STICK(11, 9), 
-		LEFT_BUMPER(5, 5), 
-		RIGHT_BUMPER(6, 6), 
-		LEFT_TRIGGER(7,-1), 
-		RIGHT_TRIGGER(8, -2), 
-		A(2, 1), 
-		B(3, 2), 
-		X(1, 3), 
-		Y(4, 4);
+		LEFT_STICK(8, 11), 
+		RIGHT_STICK(9, 10), 
+		LEFT_BUMPER(4, 4), 
+		RIGHT_BUMPER(5, 5), 
+		START(7, 9),
+		BACK(6, 8),
+
+		A(0, 1), 
+		B(1, 2), 
+		X(2, 0), 
+		Y(3, 3);
 
 		int xModeNumber, dModeNumber;
 
 		buttonMap(int xModeNumber, int dModeNumber) {
+			this.xModeNumber = xModeNumber;
+			this.dModeNumber = dModeNumber;
+		}
+
+		public int getPortNumber(JoystickMode mode) {
+			if (mode == JoystickMode.X) {
+				return xModeNumber;
+			}
+			else {
+				return dModeNumber;
+			}
+		}
+	}
+
+	public enum axisMap {
+		LEFT_AXIS_X(0, 0), 
+		LEFT_AXIS_Y(1, 1), 
+		RIGHT_AXIS_X(4, 2), 
+		RIGHT_AXIS_Y(5, 3); 
+
+		int xModeNumber, dModeNumber;
+
+		axisMap(int xModeNumber, int dModeNumber) {
+			this.xModeNumber = xModeNumber;
+			this.dModeNumber = dModeNumber;
+		}
+
+		public int getPortNumber (JoystickMode mode) {
+			if (mode == JoystickMode.X) {
+				return xModeNumber;
+			}
+			else {
+				return dModeNumber;
+			}
+		}
+	}
+	
+	public enum triggerMap {
+		LEFT_TRIGGER(7, 6),
+		RIGHT_TRIGGER(8, 7);
+		
+		int xModeNumber;
+		int dModeNumber;
+		
+		triggerMap(int xModeNumber, int dModeNumber) {
 			this.xModeNumber = xModeNumber;
 			this.dModeNumber = dModeNumber;
 		}
@@ -50,86 +95,72 @@ public class F310_Joystick {
 		}
 	}
 
-	public enum axisMap {
-		LEFT_AXIS_X(0, 0), 
-		LEFT_AXIS_Y(1, 1), 
-		LEFT_AXIS_Z(2, -1),
-		RIGHT_AXIS_X(4, 2), 
-		RIGHT_AXIS_Y(5, 3), 
-		RIGHT_AXIS_Z(3, -1);
-
-		int xModeNumber, dModeNumber;
-
-		axisMap(int xModeNumber, int dModeNumber) {
-			this.xModeNumber = xModeNumber;
-			this.dModeNumber = dModeNumber;
-		}
-		
-		public int getPortNumber (JoystickMode mode) {
-			if (mode == JoystickMode.X) {
-				return xModeNumber;
-			}
-			else {
-				return dModeNumber;
-			}
-		}
-	}
-
 	// gets the buttonMap representation of the specified number based on the
 	// current joystick mode
-	public buttonMap getButton(int feedNumber, JoystickMode mode) {
+	/*public buttonMap getButton(int feedNumber, JoystickMode mode) {
 		for (buttonMap button : buttonMap.values())
 			if (button.getPortNumber(getMode()) == feedNumber) {
 				return button;
 			}
 		return null;
-	}
+	}*/
 
-	public Joystick DRIVE_STICK;
+	private Joystick joystick;
 
 	public F310_Joystick(int port) {
-		DRIVE_STICK = new Joystick(port);
+		joystick = new Joystick(port);
 	}
-	
+
+	public F310_Joystick(Joystick j) {
+		joystick = j;
+	}
+
 	public JoystickMode getMode() {
-		//THIS MAY BE BACKWARDS, TOOK A GUESS
-		if (DRIVE_STICK.getButtonCount()>10) {
-			return JoystickMode.X;
+		if (joystick.getButtonCount()>10) {
+			return JoystickMode.D;
 		}
 		else {
-			return JoystickMode.D;
+			return JoystickMode.X;
 		}
 	}
 
 	// checks if the specified button has been pressed, returns true or false
-	public boolean getRawButton(buttonMap button) {
-			return DRIVE_STICK.getRawButton(button.getPortNumber(getMode()));
+	public boolean getButton(buttonMap button) {
+		return joystick.getRawButton(button.getPortNumber(getMode()));
 	}
 
 	// checks if the specified button has been pressed, returns true or false
 	//Sample call: DRIVE_STICK.getRawAxis(Side.LEFT, Axis.X);
-	public double getRawAxis(Side side, Axis axis) {
-			switch (side) {
-			case LEFT:
-				switch (axis) {
-				case X:
-					return DRIVE_STICK.getRawAxis(axisMap.LEFT_AXIS_X.getPortNumber(getMode()));
-				case Y:
-					return DRIVE_STICK.getRawAxis(axisMap.LEFT_AXIS_Y.getPortNumber(getMode()));
-				case Z:
-					return DRIVE_STICK.getRawAxis(axisMap.LEFT_AXIS_Z.getPortNumber(getMode()));
-				}
-			case RIGHT:
-				switch (axis) {
-				case X:
-					return DRIVE_STICK.getRawAxis(axisMap.RIGHT_AXIS_X.getPortNumber(getMode()));
-				case Y:
-					return DRIVE_STICK.getRawAxis(axisMap.RIGHT_AXIS_Y.getPortNumber(getMode()));
-				case Z:
-					return DRIVE_STICK.getRawAxis(axisMap.RIGHT_AXIS_Z.getPortNumber(getMode()));
-				}
+	public double getAxis(Stick side, Axis axis) {
+		switch (side) {
+		case LEFT:
+			switch (axis) {
+			case X:
+				return joystick.getRawAxis(axisMap.LEFT_AXIS_X.getPortNumber(getMode()));
+			case Y:
+				return joystick.getRawAxis(axisMap.LEFT_AXIS_Y.getPortNumber(getMode()));
 			}
-			return 0.0;
-		
+		case RIGHT:
+			switch (axis) {
+			case X:
+				return joystick.getRawAxis(axisMap.RIGHT_AXIS_X.getPortNumber(getMode()));
+			case Y:
+				return joystick.getRawAxis(axisMap.RIGHT_AXIS_Y.getPortNumber(getMode()));
+			}
+		}
+		return 0.0;
+
+	}
+	
+	public double getTrigger(Stick side) {
+		switch (side) {
+		case LEFT:
+				return joystick.getRawAxis(triggerMap.LEFT_TRIGGER.getPortNumber(getMode()));
+		case RIGHT:
+				return joystick.getRawAxis(triggerMap.RIGHT_TRIGGER.getPortNumber(getMode()));
+
+		}
+		return 0.0;
+
 	}
 }
