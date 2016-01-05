132efa2529b96a9863fe527023f66e0a534fba2f
Chase Haddleton <chase@chasehaddleton.com>
2016-01-05 15:54:21 -0500

    Added RotateToAngle Code

--- src/robot/commands/RotateToAngle.java
+++ src/robot/commands/RotateToAngle.java
@@ -1,6 +1,7 @@
 package robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
 import robot.Robot;
 
 /**
@@ -8,41 +9,90 @@ import robot.Robot;
  */
 public class RotateToAngle extends Command {
 
-	private final int target;
+	private int target;
+	double currentAngle;
+	double leftSpeed;
+	double rightSpeed;
+	double initSpeed = 0.5;
+	boolean rotateClockwise;
+	int time;
 	
-    public RotateToAngle(int angle) {
-    	this.target = angle;
-    	requires(Robot.chassisSubsystem);
-    }
+	public RotateToAngle(int angle, int time) {
+		requires(Robot.chassisSubsystem);
+		this.target = angle;
+		this.time = time;
+		this.setTimeout(time);
+	}
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    	// Which way am I going to turn?
-    	// Turn on the motors.
-    	double seconds = 3.0;
-    	this.setTimeout(seconds);
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		// Which way am I going to turn? Need to be able to get angle from
+		// chassis
+		// Turn on motors
+		
+		this.currentAngle = Robot.chassisSubsystem.getCurrentAngle();
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
+		double relativeTarget = target - this.currentAngle;
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-    	// If at the target then finish
-    	//or
-    	this.isTimedOut();
-        return false;
-    }
+		if ((relativeTarget < 0 && relativeTarget > -180) || (relativeTarget < 180)) { 
+			// Anticlockwise
+			this.leftSpeed = -initSpeed;
+			this.rightSpeed = initSpeed;
+			this.rotateClockwise = false;
+		} else { // clockwise!
+			this.leftSpeed = initSpeed;
+			this.rightSpeed = -initSpeed;
+			this.rotateClockwise = true;
+		}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    	// Stop the motors.
-    }
+		Robot.chassisSubsystem.setSpeed(leftSpeed, rightSpeed);
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    	end();
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		this.currentAngle = Robot.chassisSubsystem.getCurrentAngle();
+
+		if ((Math.abs(this.currentAngle - this.target) < 5) || (Math.abs(this.target - this.currentAngle) < 5)) {
+			this.leftSpeed = (!rotateClockwise) ? -initSpeed * .3 : initSpeed * .3;
+			this.rightSpeed = (!rotateClockwise) ? -initSpeed * .3 : initSpeed * .3;
+		} else if ((Math.abs(this.currentAngle - this.target) < 10)
+				|| (Math.abs(this.target - this.currentAngle) < 10)) {
+			this.leftSpeed = (!rotateClockwise) ? -initSpeed * .5 : initSpeed * .5;
+			this.rightSpeed = (!rotateClockwise) ? -initSpeed * .5 : initSpeed * .5;
+		} else if ((Math.abs(this.currentAngle - this.target) < 20)
+				|| (Math.abs(this.target - this.currentAngle) < 20)) {
+			this.leftSpeed = (!rotateClockwise) ? -initSpeed * .8 : initSpeed * .8;
+			this.rightSpeed = (!rotateClockwise) ? -initSpeed * .8 : initSpeed * .8;
+		}
+
+		Robot.chassisSubsystem.setSpeed(leftSpeed, rightSpeed);
+
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		if (this.currentAngle == target) {
+			return true;
+		} else if (this.currentAngle > target && this.rotateClockwise && this.time - 1 > 0) {
+			Scheduler.getInstance().add(new RotateToAngle(this.target, this.time - 1));
+			return true;
+		} else if (this.currentAngle > target && this.rotateClockwise && this.time -1 > 0) {
+			Scheduler.getInstance().add(new RotateToAngle(this.target, this.time - 1));
+			return true;
+		} else {
+			return (this.isTimedOut()) ? true : false;
+		}
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+		// stop motors
+		Robot.chassisSubsystem.setSpeed(0, 0);
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+
+	}
 }
