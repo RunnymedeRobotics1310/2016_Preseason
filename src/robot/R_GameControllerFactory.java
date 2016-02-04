f8be7809c535bedffb68c26af6cb2404650316f0
chand <chand@DESKTOP-427R10C.lan>
2016-02-03 19:39:20 -0500

    Fixed F310 joystick detection

--- src/robot/R_GameControllerFactory.java
+++ src/robot/R_GameControllerFactory.java
@@ -1,10 +1,12 @@
 package robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class R_GameControllerFactory {
     public static R_GameController getGameController(int port) {
 	Joystick newStick = new Joystick(port);
+	SmartDashboard.putString("",  newStick.getName());
 
 	switch (newStick.getName()) {
 	case "Controller (XBOX 360 For Windows)":
@@ -13,7 +15,7 @@ public class R_GameControllerFactory {
 	    return new R_Xbox_GameController(newStick);
 	/*case "Logitech Extreme 3D":
 	    return new R_Extreme3DPro_GameController(newStick);*/
-	case "Logitech F310":
+	case "Controller (Gamepad F310)":
 	    return new R_F310_GameController(newStick);
 	// Every XInput gamepad has a button layout similar to that of the Xbox
 	// 360's, including the Xbox One Controller, so by default assume that
