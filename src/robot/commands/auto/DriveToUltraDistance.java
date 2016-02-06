cb6100f2b7902276859789b77a8ca862d5875001
AquaBytez <anddypena@outlook.com>
2016-02-06 12:24:27 -0500

    Added direction for DriveToUltraDistance

--- src/robot/commands/auto/DriveToUltraDistance.java
+++ src/robot/commands/auto/DriveToUltraDistance.java
@@ -4,41 +4,42 @@ import robot.Robot;
 
 public class DriveToUltraDistance extends AutoGoStraightCommand {
 
+    /**
+     * The distance to drive to.
+     */
+    private double distanceSetpoint;
 
-	/**
-	 * The distance to drive to.
-	 */
-	private double distanceSetpoint;
+    /**
+     * The constructor for a new DriveToDistance command.
+     * 
+     * @param speed
+     *            The speed at which to drive.
+     * @param angle
+     *            The angle to drive at (in degrees).
+     * @param distance
+     *            The distance to drive to.
+     */
+    public DriveToUltraDistance(double speed, double angle, double distance) {
+	super(speed, angle);
+	this.distanceSetpoint = distance;
+    }
 
-	/**
-	 * The constructor for a new DriveToDistance command.
-	 * 
-	 * @param speed
-	 *            The speed at which to drive.
-	 * @param angle
-	 *            The angle to drive at (in degrees).
-	 * @param distance
-	 *            The distance to drive to.
-	 */
-	public DriveToUltraDistance(double speed, double angle, double distance) {
-		super(speed, angle);
-		this.distanceSetpoint = distance;
-	}
+    /**
+     * Gets the distance set point.
+     * 
+     * @return the distance set point.
+     */
+    public double getDistance() {
+	return distanceSetpoint;
+    }
 
-	/**
-	 * Gets the distance set point.
-	 * 
-	 * @return the distance set point.
-	 */
-	public double getDistance() {
-		return distanceSetpoint;
-	}
-
-	// Called once after isFinished returns true
-	protected boolean isFinished() {
-		
-		// Stop 4in early because it takes the robot 4 inches to stop.
-		return (distanceSetpoint-4.0 <= Robot.chassisSubsystem.getUltraSonicDistance());
-	}
+    // Called once after isFinished returns true
+    protected boolean isFinished() {
+	// Stop 4in early because it takes the robot 4 inches to stop.
+	if (getDirection() == Direction.FORWARD)
+	    return (distanceSetpoint - 4.0 < Robot.chassisSubsystem.getUltraSonicDistance());
+	else
+	    return (distanceSetpoint + 4.0 > Robot.chassisSubsystem.getUltraSonicDistance());
+    }
 
 }
