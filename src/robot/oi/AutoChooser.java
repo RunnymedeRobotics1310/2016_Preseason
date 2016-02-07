68722f95ed03dd78845d9aa5d840ff7303fda6ff
AllieWaddell <happygolucky@rogers.com>
2016-02-07 15:27:17 -0500

    Stuff idk

--- src/robot/oi/AutoChooser.java
+++ src/robot/oi/AutoChooser.java
@@ -8,22 +8,34 @@ import robot.commands.auto.DriveToLimit;
 import robot.commands.auto.DriveToUltraDistance;
 import robot.commands.auto.TestAutoCommandGroup;
 
-public class AutoChooser extends SendableChooser {
+public class AutoChooser {
 
+	SendableChooser oldChooser = new SendableChooser();
+	SendableChooser slotChooser = new SendableChooser();
+	
 	public AutoChooser() {
 		
-        addDefault("Default Auto", new DriveToDistance(0.5, 0.0, 0.5));
-        addObject("Drive To Distance Positive", new DriveToDistance(0.5, 0.0, 180.0));
-        addObject("Drive To Distance Negative", new DriveToDistance(-0.5, 0.0, 180.0));
-        addObject("Drive To Ultrasound", new DriveToUltraDistance(0.5, 0.0, 50));
-        addObject("Drive To Proximity", new DriveToLimit(0.5, 0.0));
-        addObject("Test Auto", new TestAutoCommandGroup());
+        oldChooser.addDefault("Default Auto", new DriveToDistance(0.5, 0.0, 0.5));
+        oldChooser.addObject("Drive To Distance Positive", new DriveToDistance(0.5, 0.0, 180.0));
+        oldChooser.addObject("Drive To Distance Negative", new DriveToDistance(-0.5, 0.0, 180.0));
+        oldChooser.addObject("Drive To Ultrasound", new DriveToUltraDistance(0.5, 0.0, 50));
+        oldChooser.addObject("Drive To Proximity", new DriveToLimit(0.5, 0.0));
+        oldChooser.addObject("Test Auto", new TestAutoCommandGroup());
+        
+        slotChooser.addObject("1",new Integer(1));
+        slotChooser.addObject("2",new Integer(2));
+        slotChooser.addObject("3",new Integer(3));
+        slotChooser.addObject("4",new Integer(4));
+        slotChooser.addObject("5",new Integer(5));
         
         
-        SmartDashboard.putData("Auto mode", this);
+        
+        SmartDashboard.putData("Auto mode", oldChooser);
+        SmartDashboard.putData("Slot position", slotChooser);
+        
 	}
 	
 	public Command getSelectedCommand() {
-		return (Command) super.getSelected();
+		return (Command) oldChooser.getSelected();
 	}
 }
